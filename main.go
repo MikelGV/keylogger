8a9d6e62fc38a70804c09a83f96bceca7bfd1663
MikelGv <mikelgaldosv@gmail.com>
2024-03-21 18:23:18 +0100

    device function added

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
-import "os"
+import (
+    "os"
+)
 
+// Wrapper
 type KeyLogger struct {
    fs *os.File
 }
+
+type device []string
+
+// Looks if there are any devices in the system
+func (d *device) hasDevice() {
+}
+
 
 func main() {
 }
